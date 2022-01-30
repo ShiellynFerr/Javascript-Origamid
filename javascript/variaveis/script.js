f5fbd5fe13d3fc296e1af6ee1d6b12b0390af1f4
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-30 14:28:34 -0300

    exercicio variaveis

---
+++ javascript/variaveis/script.js
@@ -0,0 +1,35 @@
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Ferreira",
+  cidade = "Guarulhos";
+
+console.log(sobrenome, cidade);
+
+//variavel sem valor
+
+var semDefinir;
+console.log(semDefinir);
+
+//declarar uma variavel com seu nome
+var nome = "Shiellyn";
+
+//declarar uma variavel com a sua idade
+var idade = 20;
+
+//declarar uma variavel com a sua comida favorita
+// e nãi atribuir valor
+var comida;
+
+//atribuir valor a sua comida favorita
+comida = "Sushi";
+
+//declarar 5 variaveis diferentes sem valor
+var animal,
+ roupa, 
+ país,
+ cor, 
+ altura;
