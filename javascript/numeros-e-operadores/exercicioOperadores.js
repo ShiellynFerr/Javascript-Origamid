13ab93cdbd01188e751ca063eb568915af7ee71a
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-30 15:37:40 -0300

    exercicio com operadores

---
+++ javascript/numeros-e-operadores/exercicioOperadores.js
@@ -0,0 +1,29 @@
+//Qual o resultado da seguinte expressão?
+ var total = 10 + 5 * 2 / 2 + 20; // 35 
+ console.log(total)
+
+ //Crie duas expressões que retornem NaN
+ var idade = " eu tenho 40 anos" / 2
+ console.log(idade)
+
+ var peso = 'meu peso é 94' / 2
+ console.log(peso)
+ 
+ //Somar a String '200' com o numero 50 e retornar 250
+
+ var stringN = '200';
+ var numero3 = 50
+ var total2 = +stringN + numero3;
+ console.log(total2)
+
+ //Incremente o numero 5 e torne o seu valor incrementado
+ var incrementar = 5
+ console.log(++incrementar)
+
+ //Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg'
+var peso = numero + unidade; //'80kg'
+var pesoPorDois = peso;
+
+console.log(pesoPorDois)
