75e49ce9f99dbd8161e1ff666d11127004f0e084
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-30 14:28:06 -0300

    exercicio tipo de dados

---
+++ javascript/tiposDeDados/script.js
@@ -0,0 +1,26 @@
+//Declare uma variavel contendo uma string
+
+var rua = "Carmem Miranda";
+
+//Declare uma variavel contendo um numero dentro de uma string
+var idade = 20;
+
+//Declare duas variaveis, uma com seu nome
+//e outra com seu sobrenome e depois some as duas
+
+var nome = "Shiellyn";
+var sobrenome = "Ferreira";
+var nomeCompleto = `${nome} ${sobrenome}`;
+// var nomeCompleto = `O nome completo dela é ${nome} ${sobrenome}`
+
+console.log( nomeCompleto )
+
+//coloque a seguinte frase em uma variavel: it's time
+
+var time = " It's time"
+
+console.log(time)
+
+//verifique o tipo da variavel que contém seu nome
+
+console.log(typeof nome)
