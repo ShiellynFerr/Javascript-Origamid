c2b7e7f472bd58f30e63f7075f97b8ec794c86eb
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-02-02 14:19:19 -0300

    exericicios de objetos feitos

--- javascript/objetos/objetos-exercicio.js
+++ javascript/objetos/objetos-exercicio.js
@@ -1,7 +1,15 @@
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
-
+var dadosPessoais = {
+  nome: 'Shiellyn',
+  sobrenome: 'Ferreira',
+  idade: 20,
+  solteira: true,
+}
 // Crie um método no objeto anterior, que mostre o seu nome completo
+dadosPessoais.nomeCompleto = function (){
+return `${this.nome} ${this.sobrenome} `
+}
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
@@ -10,6 +18,22 @@ var carro = {
   marca: 'Audi',
 }
 
+carro.preco = 3000 
+
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
 
+var cachorro = {
+  nome: 'Logan',
+  idade: 10,
+  raca: 'labrador',
+  cor: 'preto',
+  latir(pessoa) {
+ 
+    if(pessoa === 'homem'){
+      return 'AUAUUAUAUAU'
+    } else{
+      return 'silencio'
+    }
+  }
+}
