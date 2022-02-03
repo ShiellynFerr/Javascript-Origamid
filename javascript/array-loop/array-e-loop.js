fd32435fa919cca6e7c40923e4c909ac9fb6deaa
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-02-03 15:48:55 -0300

    exercicio 1 de array e loop

---
+++ javascript/array-loop/array-e-loop.js
@@ -0,0 +1,30 @@
+// array é um grupo  de valores geralmente relacionados. Servem para guardamos
+//diferentes valores em uma única variavel
+
+var videoGames = ['Switch', 'Ps4', 'Xbox'];
+
+//Métodos e propriedades de uma array
+// videoGames.pop(); // Remove o último item e retorna ele
+videoGames.push('3DS'); // Adiciona ao final da array
+// videoGames.length; // 3
+
+var ultimoItem = videoGames.pop();
+
+
+//FOR LOOP
+//Fazem algo repetidamente até que uma condição seja atingida.
+
+for (var numero = 0; numero <= 10; numero++) {
+  console.log(numero);
+}
+// Retorna de 0 a 10 no console
+
+//WHILE LOOP
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+// Retorna de 0 a 9 no console
+
