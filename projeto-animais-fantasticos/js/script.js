45d38be5e23c2b5dd60065cd20d360f310216077
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-02-09 17:38:38 -0300

    dom-seletores

---
+++ projeto-animais-fantasticos/js/script.js
@@ -0,0 +1,36 @@
+const animais = document.getElementById('animais')
+console.log(animais)
+
+// Seleciona pela classe, retorna uma HTMLCollection
+const gridSection = document.getElementsByClassName('grid-section');
+const contato = document.getElementsByClassName('grid-section contato');
+
+// Seleciona todas as UL's, retorna uma HTMLCollection
+const ul = document.getElementsByTagName('ul');
+
+// Retorna o primeiro elemento
+console.log(gridSection[0]);
+
+//SELETOR GERAL ÚNICO
+//querySelector retorna o primeiro elemento que combinar com o seu seletor CSS.
+
+const linksInternos = document.querySelector('[href^="#"]')
+
+// Busca dentro do Ul apenas
+const navItem = primeiroUl.querySelector('li');
+
+//SELETOR GERAL LISTA
+//querySelectorAll retorna todos os elementos compatíveis com o seletor CSS em uma NodeList
+
+//HTMLCOLLECTION VS NODELIST
+//A diferença está nos métodos e propriedades de ambas.
+// Além disso a NodeList retornada com querySelectorAll é estática.
+
+const titulo = document.querySelector('.titulo');
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+titulo.classList.add('grid-section');
+
+console.log(gridSectionHTML); // 4 itens
+console.log(gridSectionNode); // 3 itens
