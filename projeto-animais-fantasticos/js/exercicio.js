818bbf781f724b4d0c84d5f4c45e25e1da4441b3
Shiellyn <brunashiellyn@gmail.com>
2022-03-29 09:25:28 -0300

    exercicio

---
+++ projeto-animais-fantasticos/js/exercicio.js
@@ -0,0 +1,43 @@
+
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function handleLink (event) {
+  event.preventDefault();
+    linksInternos.forEach((link) =>{
+        link.classList.remove('ativo')
+    });
+  event.currentTarget.classList.add('ativo');
+}
+
+linksInternos.forEach((link)=> {
+
+    link.addEventListener('click', handleLink);
+});
+
+
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+const todosElementos = document.querySelectorAll('body *')
+
+function handleElemento (event) {
+  console.log(event.currentTarget)
+}
+
+todosElementos.forEach((elemento)=>{
+    elemento.addEventListener('click', handleElemento);
+})
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+function handleElemento (event) {
+  event.currentTarget.remove();
+}
+
